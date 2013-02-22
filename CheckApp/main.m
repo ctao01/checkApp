2f7d44c1e1dc15621dc34e0561662255128e9e97
ctao01 <ctao01@students.poly.edu>
2013-02-22 09:28:01 -0600

    Initial Commit

---
+++ CheckApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CheckApp
+//
+//  Created by Joy Tao on 2/22/13.
+//  Copyright (c) 2013 Joy Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JTAppDelegate class]));
+    }
+}
